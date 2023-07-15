18433051f1dc133357c4fb6aab654708aa34ec7e
maram allaghi <writtenbymaram@gmail.com>
2023-07-15 20:18:54 +0300

    created Like model and controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many_attached :images
 
-  validates :images, presence: true, blob: { content_type: :image } 
+  validates :images, presence: true, blob: { content_type: :image }
+
+  has_many :likes
+  has_many :likers, through: :likes, source: :user
 end
