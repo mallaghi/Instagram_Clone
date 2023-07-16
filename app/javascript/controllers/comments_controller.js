443923d84f7611a58f2ce8e90eec4c0460eed6c6
maram allaghi <writtenbymaram@gmail.com>
2023-07-16 15:33:13 +0300

    added comments and views

---
+++ app/javascript/controllers/comments_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="comments"
+export default class extends Controller {
+  connect() {
+  }
+
+  clear(){
+    this.element.reset();
+  }
+}
