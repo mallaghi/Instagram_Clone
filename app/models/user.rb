b8f36cac2675d05e649e9bc78e46268efad8960c
maram allaghi <writtenbymaram@gmail.com>
2023-07-31 19:52:06 +0300

    added follow model and tweaked user model to store followers/followings

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,17 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
   has_many :comments
+  has_many :follow_requests, -> { where(accepted: false) }, class_name: "Follow", foreign_key: "followed_id"
+  has_many :accepted_recieved_requests, -> { where(accepted: true) }, class_name: "Follow", foreign_key: "followed_id"
+  has_many :accepted_sent_requests, -> { where(accepted: true) }, class_name: "Follow", foreign_key: "follower_id"
+  has_many :followers, through: :accepted_recieved_requests, source: :follower
+  has_many :followings, through: :accepted_sent_requests, source: :followed
+
+  def follow(user)
+    Follow.create(follower: self, followed: user)
+  end
+
+  def unfollow(user)
+    self.accepted_sent_requests.find_by(followed: user)&.destroy
+  end
 end
