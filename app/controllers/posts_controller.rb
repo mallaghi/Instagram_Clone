061a91a6d203b464ad116e08ade70c2611a44d0a
maram allaghi <writtenbymaram@gmail.com>
2023-10-23 01:45:23 +0300

    switched to cloudinary image classification, need to bug falsey classification

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,38 +1,53 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy]
 
-  # GET /posts or /posts.json
+
   def index
     redirect_to root_path
   end
 
-  # GET /posts/1 or /posts/1.json
+
   def show
   end
 
-  # GET /posts/new
+
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  
+  def contains_animal?(uploaded_image)
+    tags_data = uploaded_image.dig('info', 'categorization', 'data', 'tags')
+    tags_data&.each { |tag| puts "#{tag['tag']}: #{tag['confidence']}" }
+    animal_tag = tags_data&.find { |tag| tag["tag"].downcase == 'animal' || tag["tag"].downcase == 'pet'}
+    !animal_tag.nil?
+  end
+
   def create
     @post = current_user.posts.new(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to root_path, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { redirect_to root_path, status: :unprocessable_entity, alert: @post.errors.full_messages }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    uploaded_image = Cloudinary::Uploader.upload(params[:post][:images].second.tempfile.path, :categorization => "aws_rek_tagging", :auto_tagging => 0.7)
+    puts "Uploaded Image: #{uploaded_image.inspect}"
+
+    contains_animal = contains_animal?(uploaded_image)
+
+    puts "CONTAINS ANIMAL:#{contains_animal}"
+
+    # respond_to do |format|
+    #   if contains_animal
+    #     @post.save
+    #     format.html { redirect_to root_path, notice: "Post was successfully created." }
+    #     format.json { render :show, status: :created, location: @post }
+    #   else
+    #     @post.errors.add(:base, "Image must contain an animal.") unless contains_animal
+    #     format.html { redirect_to root_path, status: :unprocessable_entity, alert: @post.errors.full_messages }
+    #     format.json { render json: @post.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
+
 
 
   def update
@@ -47,7 +62,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1 or /posts/1.json
   def destroy
     return if current_user != @post.user
     @post.destroy
